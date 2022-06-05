d231794ca27f4b6cf8453316b374cd8227375494
go1earner <mechetin.artur2011@yandex.com>
2022-06-05 22:27:47 +0300

    fix: type assertion

--- box.go
+++ box.go
@@ -97,7 +97,7 @@ func (b *box) SumArea() float64 {
 func (b *box) RemoveAllCircles() error {
 	isCircleHere := false
 	for index, shape := range b.shapes {
-		_, ok := shape.(Circle)
+		_, ok := shape.(*Circle)
 		if ok {
 			b.shapes = append(b.shapes[:index], b.shapes[index+1:]...)
 			isCircleHere = true
