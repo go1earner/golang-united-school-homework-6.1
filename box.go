0c533fbe8039f893a4cd9378b51003039e1aee07
go1earner <mechetin.artur2011@yandex.com>
2022-06-05 22:50:08 +0300

    fix: type if circle

--- box.go
+++ box.go
@@ -97,7 +97,7 @@ func (b *box) SumArea() float64 {
 func (b *box) RemoveAllCircles() error {
 	isCircleHere := false
 	for _, shape := range b.shapes {
-		_, ok := shape.(Circle)
+		_, ok := shape.(*Circle)
 		if ok {
 			b.shapes = b.shapes[1:]
 			isCircleHere = true
